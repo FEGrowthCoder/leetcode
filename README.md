6203a0bf3b98a8fe8a551ab0f95538a038908fb7
gaoting <ting.gao@beibei.com>
2019-10-31 15:43:13 +0800

    feat: 添加目录

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ leetcode记录
 
 # 分享内容
 - [排序专题]()
+- [test](./doc/hello.md)
 
 # 题目解析
 ### 排序专题
+### 字符串专题
+- [859亲密字符串](./code/string/859.BuddyStrings.js)
 
 # 关于Team
+来自leetcode小组
---
+++ code/string/859.BuddyStrings.js
@@ -0,0 +1,39 @@
+/**
+ * 亲密字符串
+ * https://leetcode-cn.com/problems/buddy-strings/description/
+ * 解题思路
+ * 
+ * 亲密字符串长度相等
+ * 如果是亲密字符串的话，存在0对或者2对不等的情况
+ * 如果A B相等 那么如果A/B中存在重复字符，则A B为亲密字符串
+ * 如果2对不等 则需要找出不等的索引
+ * 比较A[i] == B[j] && A[j] == B[i] && length == 2
+ * 则为亲密字符串
+ */
+
+ /**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+var buddyStrings = function(A, B) {
+  var A_len = A.length
+  var B_len = B.length
+  var diff = []
+
+  if (A_len != B_len) return false
+
+  // 如果A B相等，并且A/B中存在重复字符，说明是亲密字符串
+  if (A == B && new Set(A.split('')).size < A_len) return true
+
+  for (var i = 0; i < A_len; i++) {
+    if (A[i] !== B[i]) {
+      // 找出不同的索引
+      diff.push(i)
+    }
+  }
+
+  return (diff.length == 2 && A[diff[0]] == B[diff[1]] && A[diff[1]] == B[diff[0]])
+};
+
+console.log(buddyStrings('aaaabb', 'aaaabb'))
